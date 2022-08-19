f65bd65ece069cca6163202d3f63d58e8310004d
Sahil <samael17111@gmail.com>
2022-08-19 19:01:41 +0530

    created

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Dicee());
+}
+
+class Dicee extends StatelessWidget {
+// This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
